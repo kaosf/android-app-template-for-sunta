2b5a3d7fd8562b7fddf4cb87b63e1fd8ff21e721
ka <ka.kaosf@gmail.com>
2013-06-30 06:39:58 +0900

    enable to back with back button

--- src/jp/sunta/app/MainActivity.java
+++ src/jp/sunta/app/MainActivity.java
@@ -3,6 +3,7 @@ package jp.sunta.app;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebChromeClient;
 
@@ -26,6 +27,18 @@ public class MainActivity extends Activity {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    // Check if the key event was the Back button and if there's history
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()) {
+      wv.goBack();
+      return true;
+    }
+    // If it wasn't the Back key or there's no web page history, bubble up to the default
+    // system behavior (probably exit the activity)
+    return super.onKeyDown(keyCode, event);
   }
 
 }
